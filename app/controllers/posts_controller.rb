09df50106ef6a1bac9069d9f574526ef6fcc9df9
AJ <alexanderjessediaz@gmail.com>
2020-10-12 14:33:12 -0600

    add show, create, destroy methods to Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+
+        render json: @posts
+    end
+
+    def show
+        @post = Post.find(params[:id])
+
+        render json: @post
+    end
+
+    def create
+        @post = Post.create(
+            title: params[:title],
+            content: params[:content]
+        )
+        render json: {message: "#{@post.title} was created"}
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+
+        @post.destroy
+
+        render json: {message: "#{@post.title} was deleted"}
+
+    end
 end
