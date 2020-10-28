4c258d211a4974d3665e4df2563071a39cb41fac
AJ <alexanderjessediaz@gmail.com>
2020-10-28 16:11:21 -0600

    add create and user_params methods to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+        
+        if @user.valid?
+            @user.save 
+            @token = JWT.encode({user_id: @user.id})
+            render json: {user: @user,token: @token},status: :created
+        else 
+            render json: {error: "Username has been take"}, status: :not_acceptable
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
